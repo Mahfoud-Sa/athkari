de078f431e052f8bf6d090a058de10188797580f
Mahfoud-Sa <binsabbah2013@gmail.com>
2024-05-10 22:36:48 +0300

    add the splash screen

---
+++ lib/app/features/on_bording/presentation/pages/on_bording.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnbordingPage extends StatelessWidget {
+  const OnbordingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [Image.asset("assets/images/splash_screen.png")]);
+  }
+}
---
+++ lib/app/features/on_bording/presentation/pages/welcome_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/splash_screen.png"))),
+    );
+  }
+}
