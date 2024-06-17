8cf32de6166f77553a19aed925fa1d74d206ee12
Mahfoud-Sa <binsabbah2013@gmail.com>
2024-06-17 23:18:57 +0300

    update font size

--- lib/app/features/home/presentation/pages/home_page.dart
+++ lib/app/features/home/presentation/pages/home_page.dart
@@ -71,33 +71,35 @@ class HomePage extends StatelessWidget {
                 const SizedBox(
                   height: 10,
                 ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(25)),
-                      border: GradientBoxBorder(
-                          width: 1,
-                          gradient: LinearGradient(colors: [
-                            Color.fromARGB(255, 90, 202, 165),
-                            Color.fromARGB(255, 178, 231, 93),
-                          ]))),
-                  child: Center(
-                    child: Text(
-                        textAlign: TextAlign.center,
-                        'بسم الله الذي لا يضر مع اسمه شيء في الارض ولا في السماء وهو السميع العليم',
-                        style:
-                            Theme.of(context).textTheme.titleMedium!.copyWith(
-
-                                //   color: Color.fromARGB(255, 159, 216, 174),
-                                )),
-                  ),
-                )
+                _buildTodayDekarSection(context)
               ],
             ),
           ),
         ));
+  }
+
+  Container _buildTodayDekarSection(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(25)),
+          border: GradientBoxBorder(
+              width: 1,
+              gradient: LinearGradient(colors: [
+                Color.fromARGB(255, 90, 202, 165),
+                Color.fromARGB(255, 178, 231, 93),
+              ]))),
+      child: Center(
+        child: Text(
+            textAlign: TextAlign.center,
+            'بسم الله الذي لا يضر مع اسمه شيء في الارض ولا في السماء وهو السميع العليم',
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium!
+                .copyWith(fontSize: 22)),
+      ),
+    );
   }
 
   SingleChildScrollView _buildDekarSection() {
