38d29a36981434fe05b6453a88ebcc04a1e6726a
Mahfoud-Sa <binsabbah2013@gmail.com>
2024-06-24 11:16:22 +0300

    commit befor upgrade

--- lib/app/features/home/presentation/pages/drawer.dart
+++ lib/app/features/home/presentation/pages/drawer.dart
@@ -1,17 +1,27 @@
+import 'dart:html';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class DrawerWidget extends StatelessWidget {
-  const DrawerWidget({
+class DrawerWidget extends StatefulWidget {
+  DrawerWidget({
     super.key,
   });
 
+  @override
+  State<DrawerWidget> createState() => _DrawerWidgetState();
+}
+
+class _DrawerWidgetState extends State<DrawerWidget> {
+  bool _value = false;
   @override
   Widget build(BuildContext context) {
     return Drawer(
       width: double.infinity,
       child: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Container(
               height: 100,
@@ -49,6 +59,110 @@ class DrawerWidget extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'إعدادات التطبيق',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(fontSize: 22),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Switch(
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      dragStartBehavior: DragStartBehavior.start,
+                      splashRadius: 250,
+                      activeColor: const Color.fromARGB(255, 157, 199, 168),
+                      value: _value,
+                      onChanged: (value) {
+                        setState(() {
+                          _value = !_value;
+                        });
+                      }),
+                  Text(
+                    'الوضع الليلي',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .copyWith(color: Colors.black),
+                  )
+                ],
+              ),
+            ),
+            ExpansionTile(
+              expandedAlignment: Alignment.centerRight,
+              trailing: Text(
+                'ضبط الوقت والتاريخ',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(color: Colors.black),
+              ),
+              leading: Icon(Icons.arrow_drop_up),
+              title: Text('تلقائي'),
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('تلقائي'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('مخصص'),
+                  ],
+                ),
+              ],
+            ),
+            ExpansionPanelList(
+              expandIconColor: Colors.amberAccent,
+              children: [
+                ExpansionPanel(
+                  headerBuilder: (context, isExpanded) => Text('تلقائي'),
+                  body: Text('تلقائي'),
+                  isExpanded: true,
+                ),
+                ExpansionPanel(
+                  headerBuilder: (context, isExpanded) => Text('تلقائي'),
+                  body: Text('تلقائي'),
+                  isExpanded: false,
+                ),
+                ExpansionPanel(
+                  headerBuilder: (context, isExpanded) => Text('تلقائي'),
+                  body: Text('تلقائي'),
+                  isExpanded: false,
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: DropdownButton(
+                  icon: Text(
+                    'ضبط الوقت والتاريخ',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .copyWith(color: Colors.black),
+                  ),
+                  isExpanded: true,
+                  alignment: Alignment.centerRight,
+                  value: 0,
+                  items: [
+                    DropdownMenuItem(
+                      child: Text("تلقائي"),
+                      value: 0,
+                    ),
+                    DropdownMenuItem(child: Text("مخصص"), value: 1)
+                  ],
+                  onChanged: (value) {}),
             )
           ],
         ),
