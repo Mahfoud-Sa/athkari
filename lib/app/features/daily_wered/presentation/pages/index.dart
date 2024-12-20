37da6be23bd8183d3af48ca40a431fab3f17c42d
Mahfoud-Sa <binsabbah2013@gmail.com>
2024-12-20 16:35:44 +0300

    temp

--- lib/app/features/daily_wered/presentation/pages/index.dart
+++ lib/app/features/daily_wered/presentation/pages/index.dart
@@ -1,6 +1,12 @@
-import 'package:athkari/app/features/home/presentation/widgets/DrawerTitleWidet.dart';
+import 'package:athkari/app/features/daily_wered/presentation/pages/index.dart';
+import 'package:athkari/app/features/home/presentation/pages/drawer.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
+import 'package:gradient_borders/gradient_borders.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class IndexPage extends StatefulWidget {
   const IndexPage({super.key});
@@ -18,30 +24,48 @@ class _IndexPageState extends State<IndexPage> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              TextField(),
-              Text("عدد الاذكار"),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
-              dkeerWidget(),
+              _buildSearchBar(),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text("عدد الاذكار: ${30}")),
+              ),
+              ExpansionPanelList(
+                children: [
+                  ExpansionPanel(
+                      headerBuilder: (context, isExpanded) =>
+                          _buildTodayDekarSection(context),
+                      body: _buildTodayDekarSection(context),
+                      isExpanded: true),
+                ],
+              ),
             ],
           ),
         ));
+  }
+
+  Padding _buildSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: TextField(
+        keyboardType: TextInputType.text,
+        textAlign: TextAlign.right,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.search), // Icon inside the border
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide.none, // Remove the visible border
+          ),
+
+          hintText: '...بحث ',
+          filled: true, // Enable background color
+          fillColor: Colors.black12, // Set the background color
+          contentPadding: const EdgeInsets.symmetric(
+              vertical: 10, horizontal: 20), // Adjust padding
+        ),
+      ),
+    );
   }
 }
 
@@ -53,7 +77,6 @@ class dkeerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 250,
       decoration: BoxDecoration(border: Border.all(color: Colors.black38)),
       child: Column(children: [
         Text(
@@ -80,36 +103,60 @@ class dkeerWidget extends StatelessWidget {
   }
 }
 
-AppBar _buildAppBar(BuildContext context) {
-  return AppBar(
+Container _buildTodayDekarSection(BuildContext context) {
+  return Container(
+    margin: const EdgeInsets.symmetric(horizontal: 10),
+    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+    decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        border: GradientBoxBorder(
+            width: 1,
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 90, 202, 165),
+              Color.fromARGB(255, 178, 231, 93),
+            ]))),
+    child: Center(
+      child: Text(
+          textAlign: TextAlign.center,
+          "اللّهـمَّ أَنْتَ رَبِّـي لا إلهَ إلاّ أَنْتَ ، خَلَقْتَنـي وَأَنا عَبْـدُك ، وَأَنا عَلـى عَهْـدِكَ وَوَعْـدِكَ ما اسْتَـطَعْـت ، أَعـوذُبِكَ مِنْ شَـرِّ ما صَنَـعْت ، أَبـوءُ لَـكَ بِنِعْـمَتِـكَ عَلَـيَّ وَأَبـوءُ بِذَنْـبي فَاغْفـِرْ لي فَإِنَّـهُ لا يَغْـفِرُ الذُّنـوبَ إِلاّ أَنْتَ ",
+          style:
+              Theme.of(context).textTheme.titleMedium!.copyWith(fontSize: 22)),
+    ),
+  );
+}
+
+PreferredSize _buildAppBar(BuildContext context) {
+  return PreferredSize(
+    preferredSize: const Size.fromHeight(75), // Set custom height here
+    child: AppBar(
       flexibleSpace: Container(
-        height: 100,
         decoration: const BoxDecoration(
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
-            image: DecorationImage(
-                fit: BoxFit.fitWidth,
-                image: AssetImage("assets/images/patteren.png"))),
+          color: Colors.black38,
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(15)),
+          image: DecorationImage(
+            fit: BoxFit.fitWidth,
+            image: AssetImage("assets/images/patteren.png"),
+          ),
+        ),
+      ),
+      automaticallyImplyLeading: false, // Remove default leading button
+      centerTitle: true, // Keeps the title centered horizontally
+      title: Text(
+        "الورد اليومي",
+        style: Theme.of(context).textTheme.titleMedium!.copyWith(
+            fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
       ),
       actions: [
-        Builder(builder: (context) {
-          return IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: SvgPicture.asset('assets/svgs/back_button.svg'),
-          );
-        }),
-      ],
-      automaticallyImplyLeading: false, // Remove default leading button
-      centerTitle: true,
-      title: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          "الورد اليومي",
-          style: Theme.of(context)
-              .textTheme
-              .titleMedium!
-              .copyWith(fontSize: 22, color: Colors.white),
+        IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: SvgPicture.asset('assets/svgs/back_button.svg'),
+          padding: EdgeInsets.zero, // Remove default padding
+          constraints: const BoxConstraints(), // Remove default constraints
+          alignment: Alignment.center, // Center vertically
         ),
-      ));
+      ],
+    ),
+  );
 }
